937b845bfb8ca7b6ce6baf8945449bc5bb6ddd67
Abin John <abinjohnanto@gmail.com>
2022-11-01 08:53:49 +0530

    style: update spacing and sizing

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const priceSlider = document.getElementById('price-slider');
 const sliderProgressEl = document.querySelector('.slider-bar.--progress');
+const discountTagEl = document.querySelector('.discount-tag');
 
 function sliderChangeHandler(e) {
   const sliderValue = Number.parseInt(e.target.value);
@@ -7,8 +8,21 @@ function sliderChangeHandler(e) {
     sliderValue < 10 ? sliderValue + 3 : sliderValue
   }%`;
 
+  //   update slider color
   sliderProgressEl.style.backgroundColor =
     sliderValue === 100 ? 'hsl(174, 77%, 80%)' : 'hsl(174, 86%, 45%)';
 }
 
+function responsiveDesign() {
+  console.log(window.innerWidth);
+  if (window.innerWidth > 500) {
+    discountTagEl.innerText = '25% discount';
+  } else {
+    discountTagEl.innerText = '-25%';
+  }
+}
+
 priceSlider.addEventListener('input', sliderChangeHandler);
+addEventListener('resize', responsiveDesign);
+
+console.log(window.innerWidth);
